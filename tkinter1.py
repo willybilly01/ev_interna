a72afa18b97669bc05091eac0c37c2f6de807740
willybilly01 <william.hsu@craighouse.cl>
2024-08-13 22:02:01 -0400

    almost finish page 2

--- tkinter1.py
+++ tkinter1.py
@@ -3,9 +3,16 @@ from tkinter import *
 from tkinter.ttk import Treeview, Style
 from PIL import Image, ImageTk
 import mysql.connector
+import threading
+
+from CTkMessagebox import CTkMessagebox
+from win32gui import GetWindowText, GetForegroundWindow
+import win32process
+import psutil
+import time
+import mysql.connector
 from mysql.connector import Error
 from datetime import datetime
-from CTkMessagebox import CTkMessagebox
 
 customtkinter.set_appearance_mode('System')
 customtkinter.set_appearance_mode('Red')
@@ -13,9 +20,159 @@ customtkinter.set_appearance_mode('Red')
 def configure_treeview_style():
     style = Style()
     style.configure('Treeview',
-                    font=('Arial', 20))
+                    font=('Trebuchet', 18),  # Font for treeview items
+                    foreground='black',  # Text color
+                    background='lightblue',  # Background color
+                    rowheight=30,  # Row height
+                    fieldbackground='white',  # Entry field background color
+                    bordercolor='gray',  # Border color
+                    relief='solid'  # Border style
+                    )
     style.configure('Treeview.Heading',
-                    font=('Arial', 20, 'bold'))
+                    font=('Trebuchet', 20, 'bold'),  # Font for header
+                    foreground='black',  # Text color for header
+                    background='black'  # Background color for header
+                    )
+
+    style.configure("Treeview.Heading", relief="solid")
+
+    style.layout('Treeview', [('Treeview.treearea', {'sticky': 'nswe'})])
+    style.map("Treeview",
+              background=[("selected", "blue")],
+              foreground=[("selected", "white")])
+class Monitoreo_programa:
+    def __init__(self, user_id, blocked_programs):
+        self.user_id = user_id
+        self.blocked_programs = blocked_programs
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="8879576",
+            database="ev_interna"
+        )
+
+    def monitor(self):
+        user_id = 1
+        mycursor = self.db.cursor()
+        suma = 0
+        while True:
+            tiempo_partida = time.time()
+            ventana_fore = GetForegroundWindow()
+            id, pid = win32process.GetWindowThreadProcessId(ventana_fore)
+
+            if pid > 0:
+                try:
+                    proceso_actual = psutil.Process(pid)
+                    nombre_programa = proceso_actual.name()
+                    time.sleep(1)
+
+                    query = """ 
+                            SELECT programa_bloqueado
+                            FROM usuario_programa 
+                            JOIN programas ON usuario_programa.ID_programa = programas.ID_programa
+                            WHERE ID_usuario = %s AND nombre_programa = %s
+                    """
+                    mycursor.execute(query, (user_id, nombre_programa))
+                    result = mycursor.fetchone()
+
+                    if result[0] == 1:
+                        msg1 = CTkMessagebox(message="This program has been blocked.",
+                                             option_1="Ok",
+                                             button_width=375)
+                        if msg1.get() == "Ok":
+                            proceso_actual.terminate()
+
+                    mycursor.execute(
+                        "SELECT ID_programa "
+                        "FROM programas "
+                        "WHERE nombre_programa = %s", (nombre_programa,)
+                    )
+                    program_id = mycursor.fetchone()
+                    print("stage 1")
+
+                    if program_id is None:
+                        query_insertar = ("INSERT INTO programas (nombre_programa) "
+                                          "VALUES(%s)")
+                        mycursor.execute(query_insertar, (nombre_programa,))
+                        self.db.commit()
+                        print("stage 2")
+
+                        mycursor.execute(
+                            "SELECT ID_programa "
+                            "FROM programas "
+                            "WHERE nombre_programa = %s", (nombre_programa,)
+                        )
+                        program_id = mycursor.fetchone()
+
+                    mycursor.execute(
+                        "SELECT ID_usuario_programa "
+                        "FROM usuario_programa "
+                        "WHERE ID_usuario = %s AND ID_programa = %s",
+                        (user_id, program_id[0])
+                    )
+                    result_ID_usuario_programa = mycursor.fetchone()
+                    print("stage 3")
+
+                    if result_ID_usuario_programa is None:
+                        query_insertar1 = ("INSERT INTO usuario_programa (ID_usuario,ID_programa) "
+                                           "VALUES(%s,%s)")
+                        mycursor.execute(query_insertar1, (user_id, program_id[0],))
+                        self.db.commit()
+                        print("stage 4")
+
+                        mycursor.execute(
+                            "SELECT ID_usuario_programa "
+                            "FROM usuario_programa "
+                            "WHERE ID_usuario = %s AND ID_programa = %s",
+                            (user_id, program_id[0])
+                        )
+                        result_ID_usuario_programa = mycursor.fetchone()
+
+                    checkpoint = time.time()
+                    diferencia = (checkpoint - tiempo_partida)
+
+                    suma += diferencia
+                    solo_fecha = datetime.now().date()
+
+                    mycursor.execute(
+                        "SELECT tiempo_utilizado "
+                        "FROM tiempo_utilizado "
+                        "WHERE ID_usuario_programa = %s AND solo_fecha = %s",
+                        (result_ID_usuario_programa[0], solo_fecha)
+                    )
+                    tiempo_utilizado_existente = mycursor.fetchone()
+                    print("stage 5")
+
+                    if tiempo_utilizado_existente:
+                        tiempo_nuevo = tiempo_utilizado_existente[0] + suma
+                        mycursor.execute(
+                            "UPDATE tiempo_utilizado "
+                            "SET tiempo_utilizado = %s "
+                            "WHERE ID_usuario_programa = %s AND solo_fecha = %s",
+                            (tiempo_nuevo, result_ID_usuario_programa[0], solo_fecha)
+                        )
+                        self.db.commit()
+                        print(f"Updated tiempo_utilizado: {tiempo_nuevo}")
+                        print("stage 6")
+                        suma = 0
+
+                    else:
+                        solo_hora = datetime.now().time()
+                        mycursor.execute(
+                            "INSERT INTO tiempo_utilizado (ID_usuario_programa, tiempo_utilizado, solo_hora, solo_fecha)"
+                            "VALUES (%s,%s,%s,%s) ",
+                            (result_ID_usuario_programa[0], suma, solo_hora, solo_fecha)
+                        )
+                        self.db.commit()
+
+                        print("Registro de tiempo utilizado actualizado")
+                        suma = 0
+
+
+
+
+                except Exception as e:
+                    print(f"Ocurrió un error: {e}")
 
 class Database:
     def __init__(self, host, user, password, database):
@@ -65,6 +222,10 @@ class App:
 
         self.user_id = 1
 
+        self.monitor = Monitoreo_programa(user_id=self.user_id, blocked_programs=[])
+        self.monitor_thread = threading.Thread(target=self.monitor.monitor, daemon=True)
+        self.monitor_thread.start()
+
         self.left_frame_1 = customtkinter.CTkFrame(self.root, fg_color="transparent")
         self.left_frame_1.pack(side=LEFT, fill=Y, padx=10, pady=20)
 
@@ -100,41 +261,124 @@ class App:
         page2btn =customtkinter.CTkButton(master = self.left_frame_1, image=calendar_photo, command=self.page2, text="",width=50, height=50)
         page3btn = customtkinter.CTkButton(master = self.left_frame_1, image=check_photo, command=self.page3, text="",width=50, height=50)
 
-        #separator = Separator(root, orient='vertical')
-        #separator.place(x=70, y=0, relheight=1)
-
         page1btn.pack(pady=10)
         page2btn.pack(pady=10)
         page3btn.pack(pady=10)
 
-        self.page1text = customtkinter.CTkLabel(master=self.page1_frame, text="This is page 1", font=('InterVariable', 88))
-        self.page2text = customtkinter.CTkLabel(master=self.page2_frame, text="This is page 2", font=('InterVariable', 88))
-        self.page3text = customtkinter.CTkLabel(master=self.page3_frame, text="This is page 3", font=('InterVariable', 88))
+        self.configure_page1()
+        self.configure_page2()
+        self.configure_page3()
 
+    def configure_page1(self):
+        self.page1text = customtkinter.CTkLabel(master=self.page1_frame, text="Main Menu",
+                                                font=('Montserrat Black', 60))
         self.page1text.pack(pady=12, padx=10)
-        self.page2text.pack(pady=12, padx=10)
-        self.page3text.pack(pady=12, padx=10)
-        #page1btn.place(x=10, y=10)
-        #page2btn.place(x=10, y=80)
-        #page3btn.place(x=10, y=150)
-
-        self.page1text.pack(pady=12, padx=10)
-
-        self.right_frame_1 = customtkinter.CTkFrame(self.page1_frame, fg_color= "transparent")
-        self.right_frame_1.pack(side=RIGHT, fill=Y, padx = 10, pady=20)
+        self.right_frame_1 = customtkinter.CTkFrame(self.page1_frame, fg_color="transparent")
+        self.right_frame_1.pack(side=RIGHT, fill=Y, padx=10, pady=20)
 
         self.tree = Treeview(self.page1_frame, columns=("Program Name", "Time Used", "Date"), show='headings')
         self.tree.heading("Program Name", text="Program Name")
         self.tree.heading("Time Used", text="Time Used (Minutes)")
         self.tree.heading("Date", text="Date")
-        self.tree.pack(side = LEFT, pady=50, padx=50, fill=BOTH, expand=True)
+        self.tree.pack(side=LEFT, pady=50, padx=50, fill=BOTH, expand=True)
 
-        refresh_button = customtkinter.CTkButton(master = self.right_frame_1, text="Refresh Button", command = self.refresh_info)
-        refresh_button.pack(ipadx=10, ipady=10, padx=20,pady =10)
+        refresh_button = customtkinter.CTkButton(master=self.right_frame_1, text="Refresh Button",command=self.refresh_info)
+        refresh_button.pack(ipadx=10, ipady=10, padx=20, pady=10)
 
-        date_button = customtkinter.CTkButton(master = self.right_frame_1, text="Choose Date", command = self.choose_date)
+        date_button = customtkinter.CTkButton(master=self.right_frame_1, text="Choose Date", command=self.choose_date)
         date_button.pack(ipadx=10, ipady=10, padx=20)
 
+    def configure_page2(self):
+        self.page2text = customtkinter.CTkLabel(master=self.page2_frame, text="Bloqueo de programa",
+                                                font=('Montserrat Black', 60))
+        self.page2text.pack(pady=12, padx=10)
+
+        self.right_frame_2 = customtkinter.CTkFrame(self.page2_frame, fg_color="transparent")
+        self.right_frame_2.pack(side=RIGHT, fill=Y, padx=10, pady=20)
+        self.right_frame_2.configure(width=200, height=300)
+
+        mydb = self.db.connect()
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT programas.nombre_programa "
+                         "FROM programas "
+                         "JOIN usuario_programa ON programas.ID_programa = usuario_programa.ID_programa "
+                         "WHERE usuario_programa.ID_usuario = %s ",
+                         (self.user_id, ))
+        programs = mycursor.fetchall()
+
+        self.check_vars = {}
+
+        scroll_frame = customtkinter.CTkScrollableFrame(master=self.page2_frame, width=300, height=400)
+        scroll_frame.place(x=100, y=200)
+
+        for program in programs:
+            program_name = program[0]
+
+            # Create a checkbox for each program
+            check_var = customtkinter.StringVar(value="off")
+            self.check_vars[program_name] = check_var
+            checkbox = customtkinter.CTkCheckBox(master=scroll_frame,
+                                                 text=program_name,
+                                                 variable=check_var,
+                                                 onvalue="on",
+                                                 offvalue="off"
+                                                 )
+            checkbox.pack(anchor=W,fill=Y, padx=20, pady=5,)
+
+        confirm_button = customtkinter.CTkButton(master=self.page2_frame, text="Confirm Program Selection", command=self.confirm_selection, height=50, width=150)
+        #confirm_button.pack(padx =20, pady=20, ipadx = 20, ipady = 20)
+        confirm_button.place(x=500, y=200)
+
+        delete_button = customtkinter.CTkButton(master=self.page2_frame, text="Delete Program Blocker", command=self.delete_selection, height=50, width=150)
+        delete_button.place(x=500, y=275)
+    def confirm_selection(self):
+        selected_programs = []
+        for program_name, check_var in self.check_vars.items():
+            if check_var.get() == "on":
+                selected_programs.append(program_name)
+
+        # Now `selected_programs` contains the names of the programs that were checked.
+        print("Selected Programs:", selected_programs)
+        mydb = self.db.connect()
+        mycursor = mydb.cursor()
+        update_query  = """
+                         UPDATE usuario_programa 
+                         SET programa_bloqueado = 1
+                         WHERE ID_usuario = %s AND ID_programa= (
+                            SELECT ID_programa 
+                            FROM programas 
+                            WHERE nombre_programa = %s) """
+
+        for program_name in selected_programs:
+            mycursor.execute(update_query,(self.user_id, program_name))
+
+        mydb.commit()
+
+    def delete_selection(self):
+        selected_programs = []
+        for program_name, check_var in self.check_vars.items():
+            if check_var.get() == "on":
+                selected_programs.append(program_name)
+
+        # Now `selected_programs` contains the names of the programs that were checked.
+        print("Selected Programs:", selected_programs)
+        mydb = self.db.connect()
+        mycursor = mydb.cursor()
+        update_query = """
+                                UPDATE usuario_programa 
+                                SET programa_bloqueado = 0
+                                WHERE ID_usuario = %s AND ID_programa= (
+                                   SELECT ID_programa 
+                                   FROM programas 
+                                   WHERE nombre_programa = %s) """
+
+        for program_name in selected_programs:
+            mycursor.execute(update_query, (self.user_id, program_name))
+
+        mydb.commit()
+    def configure_page3(self):
+        self.page3text = customtkinter.CTkLabel(master=self.page3_frame, text="This is page 3", font=('InterVariable', 88))
+        self.page3text.pack(pady=12, padx=10)
     def show_frame(self, frame):
         for f in [self.page1_frame, self.page2_frame, self.page3_frame]:
             f.pack_forget()
